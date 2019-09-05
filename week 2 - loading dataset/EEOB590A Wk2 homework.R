9da4a72e538ec6ad02596cb80cd4f2c7aedbd523
Haldre Rogers <haldrerogers@haldre-2015.eeob.iastate.edu>
2019-09-05 15:28:54 -0500

    add file

---
+++ week 2 - loading dataset/EEOB590A Wk2 homework.R
@@ -0,0 +1,6 @@
+# EEOB590A 
+# Homework - 5 September 2019
+
+#develop a script that reads your dataset into R, checks the structure of the dataset, and looks at the summary of the dataset. 
+
+#As you look at your data in R, make a list of the things you think you need to do to tidy up your dataset at the top of your script
